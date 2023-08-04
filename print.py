667cdce84783a17318ac87a308a256bef2440bde
Soumyarshi Ghosh <104438803+Soumyarshi-Ghosh@users.noreply.github.com>
2023-08-04 14:32:58 +0200

    Add files via upload
    Fahrenheit to Celsius temperature converter

---
+++ print.py
@@ -0,0 +1,5 @@
+print("Fahrenheit to celcius temperature converter")
+temp_fahrenheit= input(" Enter temperature in Fahrenheit: ")
+temp_as_int= int("temp_fahrenheit")
+temp_celcius = (temp_as_int-32)/9*5
+print("temp_celcius")
